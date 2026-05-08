b407f3a0cebb64afeb13daff4e86c3042e9c5f30
massiki <fikri.amrulloh15@gmail.com>
2026-05-08 21:13:24 +0700

    add show new and featured product in home page

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
 
 class Home extends Component
@@ -12,6 +13,9 @@ class Home extends Component
     {
         $categories = Category::latest('id')->get();
         $brands = Brand::latest('id')->get();
-        return view('home', compact('categories', 'brands'));
+        $newProducts = Product::latest('id')->take(8)->get();
+        $featuredProducts = Product::where('featured', true)->latest('id')->get();
+        $saleProducts = Product::whereNotNull('sale_price')->latest('id')->get();
+        return view('home', compact('categories', 'brands', 'newProducts', 'featuredProducts', 'saleProducts'));
     }
 }
