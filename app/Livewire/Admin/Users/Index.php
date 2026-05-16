5eb77f86798b4f8e36fbe234e7de5e1dea2d061a
massiki <fikri.amrulloh15@gmail.com>
2026-05-16 20:19:04 +0700

    add show data user in adminn

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -4,12 +4,14 @@ namespace App\Livewire\Admin\Users;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use App\Models\User;
 
 class Index extends Component
 {
     #[Layout('layouts.admin')]
     public function render()
     {
-        return view('admin.users.index');
+        $users = User::latest('id')->paginate(10);
+        return view('admin.users.index', compact('users'));
     }
 }
